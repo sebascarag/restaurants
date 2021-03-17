63d8dcf2bb3ff0bbc42fe19c36711699cc0fa7a1
sebascarag <sebastiansca10@gmail.com>
2021-03-16 23:33:31 -0500

    add load image

--- components/account/InfoUser.js
+++ components/account/InfoUser.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { loadImageFromGallery } from '../../utils/helpers'
 
 export default function InfoUser({ user }) {
-    console.log(user)
+    const changePhoto = async() => {
+        const result = await loadImageFromGallery([1,1])
+        console.log(result)
+    }
     return (
         <View style={styles.container}>
             <Avatar
                 rounded
                 size="large"
+                onPress={changePhoto}
                 source={
                     user.photoURL ? { uri: photoURL } : require("../../assets/avatar-default.jpg")
                 }
